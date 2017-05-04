df5357108277e8efdca564c886a8638872c14bca
Adnan Eghtesad <adnaneghtesad@gmail.com>
2017-05-04 17:24:20 -0400

    Added all gather for all
    Added timing for evpal comput and senredc

--- EVPFFT_MPI/MPI_Build.sh
+++ EVPFFT_MPI/MPI_Build.sh
@@ -1,3 +1,3 @@
-mpif90 -Mpreprocess -DMPI  -mcmodel=medium -Mlarge_arrays  -Mr8 -r8 -Msave -DUSE_FOURN -fast -tp=p7 -lrt evpnew10.for fourn.c -o EVPFFT 
+mpif90 -Mpreprocess -DMPI -DNOBarrier -DNOSENDRECV -DGATHER -mcmodel=medium -Mlarge_arrays  -Mr8 -r8 -Msave -DUSE_FOURN -fast -tp=p7 -lrt evpnew10.for fourn.c -o EVPFFT 
  
 
