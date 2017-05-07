b09f13f881857bda868927b74aaf9242f0488246
Adnan Eghtesad <adnaneghtesad@gmail.com>
2017-05-06 20:21:51 -0400

    Now actually MPI works perferct!
    Identical results for fields!
    No NAN problem!
    No problem when using 0.1 increment instead
    of 0.005 !

--- EVPFFT_MPI/MPI_Build.sh
+++ EVPFFT_MPI/MPI_Build.sh
@@ -1,4 +1,5 @@
 set -x
-mpif90 -Mpreprocess -DMPI -DBarrier -DSENDRECV -DNOGATHER -mcmodel=medium -Mlarge_arrays  -Mr8  -Msave -DUSE_FOURN -fast -tp=p7 -lrt evpnew10.for fourn.c -o EVPFFT 
+#mpif90 -Mpreprocess -DMPI -DBarrier -DSENDRECV -DNOGATHER -mcmodel=medium -Mlarge_arrays  -Mr8  -Msave -DUSE_FOURN -fast -tp=p7 -lrt evpnew10.for fourn.c -o EVPFFT 
+mpif90 -Mpreprocess -DMPI -DBarrier -DSENDRECV -DNOGATHER -mcmodel=medium -Mlarge_arrays  -Mr8  -Msave -DUSE_FOURN -fast -tp=p7 -lrt evpnew10V3.for fourn.c -o EVPFFT 
 #mpif90 -Mpreprocess -DMPI -DNOBarrier -DSENDRECV -DNOGATHER  -DUSE_FOURN -fast -r8 -tp=p7 -lrt evpnew10.for fourn.c -o EVPFFT 
 #mpif90 -Mpreprocess -DMPI -DNOBarrier -DSENDRECV -DNOGATHER  -DUSE_FOURN -fast  -tp=p7 -lrt evpnew10_Revert_to_Common_Block.for fourn.c -o EVPFFT 
--- EVPFFT_MPI/build_serial.sh
+++ EVPFFT_MPI/build_serial.sh
@@ -1,7 +1,12 @@
 set -x
 
+#pgf90  -Mpreprocess -mcmodel=medium -Mlarge_arrays  -Mr8  -Msave -DUSE_FOURN -fast -tp=p7 -lrt evpnew10.for fourn.c -o EVPFFT_serial
+pgf90  -Mpreprocess -mcmodel=medium -Mlarge_arrays  -Mr8  -Msave -DUSE_FOURN -fast -tp=p7 -lrt evpnew10V3.for fourn.c -o EVPFFT_serial
+
+
 #pgf90 -DUSE_FOURN   -Mpreprocess -Minfo=accel -ta=host -fast -tp=p7 -lrt evpnew10.for  fourn.c 
-pgf90 -DUSE_FOURN  -mcmodel=medium -Mlarge_arrays -Mr8 -r8 -Msave -Mpreprocess -Minfo=accel -ta=host -fast -tp=p7 -lrt evpnew10.for  fourn.c 
+#pgf90 -DUSE_FOURN  -mcmodel=medium -Mlarge_arrays -Mr8   -Msave -Mpreprocess -Minfo=accel -ta=host -fast -tp=p7 -lrt evpnew10.for  fourn.c 
+
 #pgf90 -DUSE_FOURN  -mcmodel=medium -Mlarge_arrays -Mcudalib=cufft -ta=tesla:pinned,fastmath -Mr8 -r8 -Msave -Mpreprocess -Minfo=accel -ta=host -fast -tp=p7 -lrt -c  evpnew10.for 
 #pgcc -DUSE_FOURN  -mcmodel=medium -Mlarge_arrays  -Mcudalib=cufft -ta=tesla:pinned,fastmath  -Mpreprocess -Minfo=accel -ta=host -fast -tp=p7 -lrt -c fourn.c
 #pgf90 evpnew10.o fourn.0 -o a.out
